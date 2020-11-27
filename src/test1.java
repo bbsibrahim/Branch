394f6dac8a39dc308668ab4466ad007b58d11a22
bbsibrahim <bbsibrahim@gmail.com>
2020-11-27 00:50:47 +0000

    3

--- src/test1.java
+++ src/test1.java
@@ -2,5 +2,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("Urunleri listele kismi yazildi");
         System.out.println("2");
+        System.out.println("3");
     }
 }
